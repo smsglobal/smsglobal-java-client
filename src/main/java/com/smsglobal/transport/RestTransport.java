df301f5bf86f825cf4a5f16b339023dbf66e6242
Paul Beard <paul.beard@netvalue.nz>
2021-10-01 12:50:22 +1300

    Improved exception handling.

--- src/main/java/com/smsglobal/transport/RestTransport.java
+++ src/main/java/com/smsglobal/transport/RestTransport.java
@@ -7,6 +7,7 @@ import com.smsglobal.client.Contact;
 import com.smsglobal.client.ContactGroups;
 import com.smsglobal.client.CreditBalance;
 import com.smsglobal.client.DedicatedNumbers;
+import com.smsglobal.client.HttpStatusCodeException;
 import com.smsglobal.client.IncomingMessages;
 import com.smsglobal.client.LowBalanceAlerts;
 import com.smsglobal.client.Message;
@@ -115,6 +116,7 @@ public class RestTransport implements Closeable {
 
     public String getMac(
         final String method, final String pathAndQuery, final long timestamp, final int nonce) throws NoSuchAlgorithmException, InvalidKeyException {
+
         final Mac mac = Mac.getInstance("HmacSHA256");
         final SecretKeySpec secretHash = new SecretKeySpec(this.secret.getBytes(), "HmacSHA256");
         mac.init(secretHash);
@@ -138,31 +140,53 @@ public class RestTransport implements Closeable {
         return getAuthHeader(mac, timestamp, nonce);
     }
 
-    private <T> T get(final String path, final List<NameValuePair> query, final Class<T> responseType) throws Exception {
+    private static void checkResponse(final CloseableHttpResponse httpResponse) throws HttpStatusCodeException {
+        final StatusLine statusLine = httpResponse.getStatusLine();
+        final int statusCode = statusLine.getStatusCode();
+        if (statusCode == 200) {
+            return;
+        }
+
+        final String reasonPhrase = statusLine.getReasonPhrase();
+        String body = null;
+        try {
+            body = EntityUtils.toString(httpResponse.getEntity());
+        } catch (final IOException ignored) {
+        }
+
+        throw new HttpStatusCodeException(statusCode, reasonPhrase, body);
+    }
+
+    private <T> T get(
+        final String path, final List<NameValuePair> query,
+        final Class<T> responseType) throws HttpStatusCodeException, IOException, URISyntaxException, NoSuchAlgorithmException, InvalidKeyException {
+
         final String pathAndQuery = query != null && !query.isEmpty() ? new URIBuilder(path).addParameters(query).toString() : path;
         final HttpGet httpRequest = new HttpGet(this.baseUrl + pathAndQuery);
         httpRequest.setHeader(HttpHeaders.ACCEPT, "application/json");
         httpRequest.setHeader(HttpHeaders.AUTHORIZATION, getAuthHeader(httpRequest.getMethod(), pathAndQuery));
         try (final CloseableHttpResponse httpResponse = this.httpClient.execute(httpRequest)) {
-            final StatusLine statusLine = httpResponse.getStatusLine();
-            if (statusLine.getStatusCode() != 200) {
-                throw new RuntimeException(
-                    statusLine.getStatusCode() + " " + statusLine.getReasonPhrase() + " " + EntityUtils.toString(httpResponse.getEntity()));
-            }
+            checkResponse(httpResponse);
 
             return this.objectMapper.readValue(httpResponse.getEntity().getContent(), responseType);
         }
     }
 
-    private <T> T get(final String path, final Class<T> responseType) throws Exception {
+    private <T> T get(
+        final String path,
+        final Class<T> responseType) throws HttpStatusCodeException, IOException, URISyntaxException, NoSuchAlgorithmException, InvalidKeyException {
+
         return get(path, null, responseType);
     }
 
-    public AutoTopup getAutoTopup() throws Exception {
+    public AutoTopup getAutoTopup() throws HttpStatusCodeException, IOException, URISyntaxException, NoSuchAlgorithmException, InvalidKeyException {
         return get("/auto-topup", AutoTopup.class);
     }
 
-    public ContactGroups getContactGroups(final Integer offset, final Integer limit) throws Exception {
+    public ContactGroups getContactGroups(
+        final Integer offset,
+        final Integer limit) throws HttpStatusCodeException, IOException, URISyntaxException, NoSuchAlgorithmException, InvalidKeyException {
+
         final List<NameValuePair> query = offset == null && limit == null ? null : new ArrayList<>(2);
         if (offset != null) {
             query.add(new BasicNameValuePair("offset", offset.toString()));
@@ -173,11 +197,14 @@ public class RestTransport implements Closeable {
         return get("/group", query, ContactGroups.class);
     }
 
-    public DedicatedNumbers getDedicatedNumbers() throws Exception {
+    public DedicatedNumbers getDedicatedNumbers() throws HttpStatusCodeException, IOException, URISyntaxException, NoSuchAlgorithmException, InvalidKeyException {
         return get("/dedicated-number", DedicatedNumbers.class);
     }
 
-    public Optouts getOptOuts(final Integer offset, final Integer limit) throws Exception {
+    public Optouts getOptOuts(
+        final Integer offset,
+        final Integer limit) throws HttpStatusCodeException, IOException, URISyntaxException, NoSuchAlgorithmException, InvalidKeyException {
+
         final List<NameValuePair> query = offset == null && limit == null ? null : new ArrayList<>(2);
         if (offset != null) {
             query.add(new BasicNameValuePair("offset", offset.toString()));
@@ -188,11 +215,14 @@ public class RestTransport implements Closeable {
         return get("/opt-outs", query, Optouts.class);
     }
 
-    public SharedPools getSharedPools() throws Exception {
+    public SharedPools getSharedPools() throws HttpStatusCodeException, IOException, URISyntaxException, NoSuchAlgorithmException, InvalidKeyException {
         return get("/sharedpool", SharedPools.class);
     }
 
-    public OutgoingMessages getOutgoingMessages(final Integer offset, final Integer limit) throws Exception {
+    public OutgoingMessages getOutgoingMessages(
+        final Integer offset,
+        final Integer limit) throws HttpStatusCodeException, IOException, URISyntaxException, NoSuchAlgorithmException, InvalidKeyException {
+
         final List<NameValuePair> query = offset == null && limit == null ? null : new ArrayList<>(2);
         if (offset != null) {
             query.add(new BasicNameValuePair("offset", offset.toString()));
@@ -203,7 +233,7 @@ public class RestTransport implements Closeable {
         return get("/sms", query, OutgoingMessages.class);
     }
 
-    public OutgoingMessages sendMessage(final Message message) throws Exception {
+    public OutgoingMessages sendMessage(final Message message) throws IOException, NoSuchAlgorithmException, InvalidKeyException, HttpStatusCodeException {
         final String messageXml = this.objectMapper.writeValueAsString(message);
         final String path = "/sms";
         final HttpPost httpRequest = new HttpPost(this.baseUrl + path);
@@ -213,17 +243,16 @@ public class RestTransport implements Closeable {
         entity.setContentType("application/json");
         httpRequest.setEntity(entity);
         try (final CloseableHttpResponse httpResponse = this.httpClient.execute(httpRequest)) {
-            final StatusLine statusLine = httpResponse.getStatusLine();
-            if (statusLine.getStatusCode() != 200) {
-                throw new RuntimeException(
-                    statusLine.getStatusCode() + " " + statusLine.getReasonPhrase() + " " + EntityUtils.toString(httpResponse.getEntity()));
-            }
+            checkResponse(httpResponse);
 
             return this.objectMapper.readValue(httpResponse.getEntity().getContent(), OutgoingMessages.class);
         }
     }
 
-    public IncomingMessages getIncomingMessages(final Integer offset, final Integer limit) throws Exception {
+    public IncomingMessages getIncomingMessages(
+        final Integer offset,
+        final Integer limit) throws HttpStatusCodeException, IOException, URISyntaxException, NoSuchAlgorithmException, InvalidKeyException {
+
         final List<NameValuePair> query = offset == null && limit == null ? null : new ArrayList<>(2);
         if (offset != null) {
             query.add(new BasicNameValuePair("offset", offset.toString()));
@@ -234,23 +263,23 @@ public class RestTransport implements Closeable {
         return get("/sms-incoming", query, IncomingMessages.class);
     }
 
-    public Contact getUserBillingDetails() throws Exception {
+    public Contact getUserBillingDetails() throws HttpStatusCodeException, IOException, URISyntaxException, NoSuchAlgorithmException, InvalidKeyException {
         return get("/user/billing-details", Contact.class);
     }
 
-    public Contact getUserContactDetails() throws Exception {
+    public Contact getUserContactDetails() throws HttpStatusCodeException, IOException, URISyntaxException, NoSuchAlgorithmException, InvalidKeyException {
         return get("/user/contact-details", Contact.class);
     }
 
-    public CreditBalance getUserCreditBalance() throws Exception {
+    public CreditBalance getUserCreditBalance() throws HttpStatusCodeException, IOException, URISyntaxException, NoSuchAlgorithmException, InvalidKeyException {
         return get("/user/credit-balance", CreditBalance.class);
     }
 
-    public LowBalanceAlerts getUserLowBalanceAlerts() throws Exception {
+    public LowBalanceAlerts getUserLowBalanceAlerts() throws HttpStatusCodeException, IOException, URISyntaxException, NoSuchAlgorithmException, InvalidKeyException {
         return get("/user/low-balance-alerts", LowBalanceAlerts.class);
     }
 
-    public VerifiedNumbers getUserVerifiedNumbers() throws Exception {
+    public VerifiedNumbers getUserVerifiedNumbers() throws HttpStatusCodeException, IOException, URISyntaxException, NoSuchAlgorithmException, InvalidKeyException {
         return get("/user/verified-numbers", VerifiedNumbers.class);
     }
 }
